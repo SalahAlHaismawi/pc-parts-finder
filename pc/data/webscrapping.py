73bda8a2ec9d3178f84add71522afe831118eab4
Salah AH <salah322s1@gmail.com>
2022-03-29 20:38:47 +0800

    .

--- pc/data/webscrapping.py
+++ pc/data/webscrapping.py
@@ -5,5 +5,10 @@ destenation ='https://www.newegg.com/p/pl?d='+userinput
 print(destenation)
 html_text = requests.get(destenation).text
 soup= BeautifulSoup(html_text, 'lxml')
-gpu= soup.find_all('strong', class_='price-current')
+item_cell=soup.find('div',class_='item-cell')
+
+gpu=item_cell.find('a',class_='item-title').text
+gpu_price=item_cell.find('li',class_='price-current').text.split(" ",0)[0].split("(")[0]
+
 print(gpu)
+print(gpu_price)
